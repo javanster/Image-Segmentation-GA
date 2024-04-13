a69e2fd3cc430dfc791f7053bc695b64a03dad14
Jarl Sterkenburg <jarl.sterkenburg@proton.me>
2024-04-13 16:32:38 +0200

    Create Parameter class
    Static class for holding parameters for the GA

---
+++ imgseg-ga/src/main/java/com/p3/Parameters.java
@@ -0,0 +1,9 @@
+package com.p3;
+
+public class Parameters {
+    public static int POPULATION_SIZE;
+    public static int PARENT_COUNT;
+    public static int TOURNAMENT_SIZE;
+    public static boolean IS_TOURNAMENT_REPLACEMENT_ALLOWED;
+    public static int GENERATIONS;
+}
