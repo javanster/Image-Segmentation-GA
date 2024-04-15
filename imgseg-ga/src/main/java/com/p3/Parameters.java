f281bb1383bac8c4c329348b52de5569537a412a
Jarl Sterkenburg <jarl.sterkenburg@proton.me>
2024-04-15 08:53:21 +0200

    Add parameter for crossoverhandler

--- imgseg-ga/src/main/java/com/p3/Parameters.java
+++ imgseg-ga/src/main/java/com/p3/Parameters.java
@@ -1,9 +1,15 @@
 package com.p3;
 
+import com.p3.interfaces.CrossoverHandler;
+
+/**
+ * A class that holds the parameters for the genetic algorithm.
+ */
 public class Parameters {
     public static int POPULATION_SIZE;
     public static int PARENT_COUNT;
     public static int TOURNAMENT_SIZE;
     public static boolean IS_TOURNAMENT_REPLACEMENT_ALLOWED;
     public static int GENERATIONS;
+    public static CrossoverHandler CROSSOVER_HANDLER;
 }
