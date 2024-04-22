7c9fa865959b3fa2f00ea449ca2e2739eaabdd99
Jarl Sterkenburg <jarl.sterkenburg@proton.me>
2024-04-22 14:02:36 +0200

    Update Parameter class with new parameters

--- imgseg-ga/src/main/java/com/p3/Parameters.java
+++ imgseg-ga/src/main/java/com/p3/Parameters.java
@@ -1,15 +1,19 @@
 package com.p3;
 
 import com.p3.interfaces.CrossoverHandler;
+import com.p3.interfaces.MutationHandler;
 
 /**
  * A class that holds the parameters for the genetic algorithm.
  */
 public class Parameters {
+    public static String IMAGE_FILE_PATH;
+    public static int SEGMENTS_LOWEBOUND;
+    public static int SEGMENTS_UPPERBOUND;
     public static int POPULATION_SIZE;
-    public static int PARENT_COUNT;
     public static int TOURNAMENT_SIZE;
     public static boolean IS_TOURNAMENT_REPLACEMENT_ALLOWED;
     public static int GENERATIONS;
     public static CrossoverHandler CROSSOVER_HANDLER;
+    public static MutationHandler MUTATION_HANDLER;
 }
