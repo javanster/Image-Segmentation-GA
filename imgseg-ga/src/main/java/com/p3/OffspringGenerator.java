81597232a138563c3db92d8e15f7f7eb0afff528
Jarl Sterkenburg <jarl.sterkenburg@proton.me>
2024-04-22 14:04:40 +0200

    Add documentation to OffspringGenerator class

--- imgseg-ga/src/main/java/com/p3/OffspringGenerator.java
+++ imgseg-ga/src/main/java/com/p3/OffspringGenerator.java
@@ -1,7 +1,18 @@
 package com.p3;
 
+/**
+ * A class that generates offspring from two parents by performing crossover and mutation.
+ */
 public class OffspringGenerator {
     
+    /**
+     * Generates offspring from two parents by performing crossover and mutation. Crossover and mutation
+     * is done according to the parameters set in the Parameters class.
+     * 
+     * @param parent1 The first parent.
+     * @param parent2 The second parent.
+     * @return An array of two individuals, which are the offspring of the two parents.
+     */
     public static Individual[] generateOffspring(Individual parent1, Individual parent2) {
 
         Individual[] offspring = Parameters.CROSSOVER_HANDLER.cross(parent1, parent2);
